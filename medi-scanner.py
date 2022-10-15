2bafff8c162aa045cb4ae895c7949524f84b3a8d
jeffreyli0312 <jeffreyli0312@gmail.com>
2022-10-14 23:31:07 -0400

    new

--- medi-scanner.py
+++ medi-scanner.py
@@ -2,7 +2,19 @@ import numpy as np
 import cv2
 
 def main():
-    print ('hello world')
+    cap = cv2.VideoCapture(0)
+    ptime = 0
+    ctime = 0
+
+    while True:
+        success , img = cap.read()
+        img_iso = np.empty(img.shape)
+        img_iso.fill(0)
+
+        cv2.imshow("Capture" , img)
+
+        if cv2.waitKey(1) &0xFF == ord('x'):
+            break # Press 'x' to close window
 
 if __name__ == '__main__':
     main()
